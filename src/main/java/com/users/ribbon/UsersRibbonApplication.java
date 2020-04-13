b01dde18a5597fd12b944dcf106c611eb543d241
unknown <sridevi.tolety15@gmail.com>
2020-04-13 19:34:02 +0530

    UserRibbon

--- src/main/java/com/users/ribbon/UsersRibbonApplication.java
+++ src/main/java/com/users/ribbon/UsersRibbonApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;​
 import org.springframework.cloud.openfeign.EnableFeignClients;​
 ​
 @SpringBootApplication​
-@EnableFeignClients("com.users.ribbon")​
+@EnableFeignClients("com.users.ribbon")
+@EnableEurekaClient​
 public class UsersRibbonApplication {​
 ​
 public static void main(String[] args) {​
